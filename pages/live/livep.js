6c9a50c9638d8df0cfb35dda3d545181d8543ef1
mingquan.li <mingquan.li@5262.com>
2018-04-23 09:05:39 +0800

    选项卡页面并模拟选项卡数据

--- pages/live/livep.js
+++ pages/live/livep.js
@@ -1,18 +1,40 @@
 // pages/live/livep.js
+var sliderWidth = 96;
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-  
+    tabs: ["选项一", "选项二","选项三"],
+    activeIndex: 1,
+    sliderOffset: 0,
+    sliderLeft: 0
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    var that = this;
+    wx.getSystemInfo({
+      success: function(res) {
+        that.setData({
+          sliderLeft: (res.windowWidth / that.data.tabs.length - sliderWidth) / 2,
+          sliderOffset: res.windowWidth / that.data.tabs.length * that.data.activeIndex
+        });
+      },
+    })
   
+  },
+  /**
+   * 点击选项卡事件
+   */
+  tabClick: function (e) {
+    this.setData({
+      sliderOffset: e.currentTarget.offsetLeft,
+      activeIndex: e.currentTarget.id
+    });
   },
 
   /**
